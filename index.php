2ec75da47c2889c7b5719f2a54aaab3deb87019e
Yousef <136933285+AmrYsff@users.noreply.github.com>
2025-01-27 18:17:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,12 @@ require 'loggedin.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="description" content="Mohamed Mousa Amro and his partners company is a partnership Company Registered in the Ministry of
+                    Industry and Trade under the name of (Alfakherah traiding), this company Specialize in importing food products and sell in the wholesale market">
+    <meta name="keywords" content="alfakhira trading, alfakhera trading, wholesale company, hajeh rice, princesses rice, Mohammad Amro company">
+    <meta name="keywords" content="رز الاميرات, رز الحجة, شركة محمد موسى عمرو, طن ريو جاردن, وادي الرمم, مواد تموينية, مواد غذائية, تجارة بالجملة">
+    <meta name="author" content="Yousef Amr">
+    <title>Alfakhera trading group</title>
     <link rel="stylesheet" href="css/reset.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
